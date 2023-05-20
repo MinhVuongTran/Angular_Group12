5830f8b38fa97684c104bd2489d7230f8bb57538
NgocTuyen0305 <tuyenln0305@gmail.com>
2023-05-21 00:44:54 +0700

    feat: get id from url,UI base product detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {SplitterModule} from 'primeng/splitter'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -56,7 +57,8 @@ import { BannerComponent } from './components/banner/banner.component';
     SlideMenuModule,
     GalleriaModule,
     CardModule,
-    CarouselModule
+    CarouselModule,
+    SplitterModule
   ],
   providers: [SliderService],
   bootstrap: [AppComponent],
